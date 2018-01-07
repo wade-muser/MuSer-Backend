b24bd0ef8b2bcff9edaf26458c3e20f85cd074a4
cypmaster14 <lila.lazar@info.uaic.ro>
2018-01-07 02:09:26 +0200

    Add Sparql Select/Insert Sample

---
+++ samples/sparql.js
@@ -0,0 +1,43 @@
+const {
+    SparqlClient,
+    SPARQL
+} = require('sparql-client-2');
+const GRAPHDB_SELECT_SPARQL_ENDPOINT = "http://localhost:7200/repositories/MuSer";
+const GRAPHDB_INSERT_SPARQL_ENDPOINT = "http://localhost:7200/repositories/MuSer/statements";
+const client = new SparqlClient(GRAPHDB_SELECT_SPARQL_ENDPOINT, {
+    updateEndpoint: GRAPHDB_INSERT_SPARQL_ENDPOINT
+});
+
+
+function selectExample() {
+
+    return client.query(SPARQL `
+    PREFIX muser: <http://example.com/muser#>
+    SELECT ?spotifyId{
+        muser:GEazy muser:idSpotify ?spotifyId.
+    }
+    `).execute()
+        .then(response => {
+            return JSON.stringify(response.results.bindings[0].spotifyId.value);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+function insertExample(value) {
+    return client.query(SPARQL `
+    PREFIX muser: <http://example.com/muser#>
+
+    INSERT DATA {
+        muser:GEazy muser:musicalArtistCountry ?value
+    }`)
+        .bind("value", value)
+        .execute()
+        .then(response => {
+            console.log("Insert was succesfully executed");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}

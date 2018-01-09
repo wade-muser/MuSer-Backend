9f87f853bd88172d41655508024da28024fd9378
xSkyripper <alexandru.cristea604@gmail.com>
2018-01-09 22:27:31 +0200

    Created SparqlService generic class

---
+++ services/commons/sparql_service.js
@@ -0,0 +1,67 @@
+const { SparqlClient, SPARQL } = require('sparql-client-2');
+
+class SparqlService {
+    constructor(
+        sparqlEndpoint,
+        sparqlEndpointUpdate,
+        prefixesSet,
+    ) {
+        this.sparqlEndpoint = sparqlEndpoint;
+        this.sparqlEndpointUpdate = sparqlEndpointUpdate;
+        this.prefixesSet = prefixesSet;
+
+        this.initClient();
+    };
+
+    initClient() {
+        this.client = new SparqlClient(this.sparqlEndpoint, {
+            defaultParameters: { format: 'json', },
+            updateEndpoint: this.sparqlEndpointUpdate
+        });
+    };
+
+    getQuery(statement) {
+        let prefixesString = '';
+        let query = '';
+        let builtQuery;
+    
+        this.prefixesSet.forEach(prefixes => {
+            Object.keys(prefixes).forEach((key, index) => {
+                prefixesString += `PREFIX ${key}: ${prefixes[key]}\n`;
+            });
+        });
+        
+
+        query = `${prefixesString}${statement}`;
+        builtQuery = this.client.query(`${query}`);
+        return builtQuery;
+    }
+
+    getQueryInsert(statements) {
+        let insertQuery;
+        let statementsString = '';
+    
+        statements.forEach(st => {
+            statementsString += `${st} .\n`
+        });
+        
+        insertQuery = this.getQuery(`INSERT DATA { ${statementsString} }`);
+        return insertQuery;
+    };
+
+    static parseQueryResult(result) {
+        let cleanResult = {};
+    
+        Object.keys(result).forEach(key => {
+            cleanResult[key] = result[key]['value'];
+        });
+        return cleanResult;
+    };
+
+    stringTemplate(literal, params) {
+        return new Function(params, "return `" + literal + "`;");
+    };
+
+};
+
+module.exports = SparqlService;

d27b0f54803e80364ba296344868ca3c91aa612b
xSkyripper <alexandru.cristea604@gmail.com>
2018-01-10 00:58:51 +0200

    Added ArtistInfo and SongsForArtist in DbpediaService

--- services/commons/dbpedia_service.js
+++ services/commons/dbpedia_service.js
@@ -2,101 +2,31 @@ const SPARQL_ENDPOINT = 'http://dbpedia.org/sparql';
 const COMMON_PREFIXES = require('./mappings').prefixes.common;
 const DBP_PREFIXES = require('./mappings').prefixes.dbp;
 const SparqlService = require('./sparql_service');
+const SparqlQueryFactory = require('./sparql_query_factory');
 
 class DbpediaService extends SparqlService {
     constructor() {
         super(SPARQL_ENDPOINT, undefined, [COMMON_PREFIXES, DBP_PREFIXES]);
+        
+        this.sparqlQueryFactory = new SparqlQueryFactory();
     };
 
-    getArtist(dbpInstance, callback) {
-        let getArtistQuery = this.getQuery();
+    getQueryArtistInfo(dbpArtist, callback) {
+        let queryArtistInfo = this.getQuery(
+            this.sparqlQueryFactory(SparqlQueryFactory.ARTIST_INFO, dbpArtist)
+        );
 
+        return queryArtistInfo;
     };
+
+    getQuerySongsForArtist(dbpArtist) {
+        let querySongsForArtist = this.getQuery(
+            this.sparqlQueryFactory(SparqlQueryFactory.SONGS_FOR_ARTIST, dbpArtist)
+        );
+
+        return querySongsForArtist;
+    }
 };
 
 
-function getArtist(dbpInstance) {
-
-    getQuerySelect(
-            )
-        .execute()
-        .then(res => {
-            console.log(parseQueryResult(res.results.bindings[0]));
-
-            return parseQueryResult(res.results.bindings[0]);
-        })
-        .then(artist => {
-            // let statements = [];
-            // Object.keys(artist).forEach(prop => {
-            //     statements.append('');
-            // });
-
-            GraphdbMuserService.insertStatements(
-                [
-                    `muser:${artist.artistName} rdf:type muser:MusicalArtist`,
-                    `muser:${artist.artistName} owl:sameAs <${artist.wikidataArtist}>`,
-                    `muser:${artist.artistName} muser:inceptionDate '${artist.artistActivityStartYear}'^^xsd:date`
-                ]
-            );
-        })
-        .catch(err => {
-            console.error(err);
-        });
-}
-
-function getSong(id) {
-
-}
-
-function getSongs(artist) {
-
-}
-
-function getAlbums(song) {
-
-}
-
-function getGenres(song) {
-
-}
-
-function getGenres(album) {
-
-}
-
-function insertMuserStatements(statements) {
-
-}
-
-function populateMuser() {
-    // get artist info
-    getArtist('dbr:Eminem');
-    // insert artist info
-    // get songs for artist
-    // insert song <-> artist
-    // get songs info 
-    // insert songs info
-    // get albums for songs
-    // insert albums <-> songs
-    // get albums info
-    // insert albums info
-    // get artists for albums
-    // insert artists <-> albums
-    // get genres for songs
-    // insert genres <-> songs
-    // get genres info
-    // insert genres info
-    // get genres for albums
-    // insert genres <-> albums
-    // get genres info
-    // insert genres info
-    // union artist genres (albums genres + songs genres)
-    // insert artist <-> genres
-
-    // get related artists for artist
-    // insert related artists
-    // get related genres for genres
-    // insert related genres
-}
-
-populateMuser();
+module.exports = DbpediaService;

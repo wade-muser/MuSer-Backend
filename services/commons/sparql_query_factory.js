8a8504a06cea2cf7c2fffb0e1015c6d25adb3a32
cypmaster14 <lila.lazar@info.uaic.ro>
2018-01-09 23:57:51 +0200

    Remove test case from Sparql Query Factory

--- services/commons/sparql_query_factory.js
+++ services/commons/sparql_query_factory.js
@@ -1,31 +1,31 @@
 const fs = require('fs');
-const genre_info_query = fs.readFileSync("queries/dbp/genre_info.rq",'utf-8');
-const genres_for_song_query = fs.readFileSync("queries/dbp/genres_for_song.rq",'utf-8');
+const genre_info_query = fs.readFileSync("queries/dbp/genre_info.rq", 'utf-8');
+const genres_for_song_query = fs.readFileSync("queries/dbp/genres_for_song.rq", 'utf-8');
 
 
 function stringTemplate(literal, params) {
     return new Function(params, "return `" + literal + "`;");
-};
+}
 
 const GENRE_INFO = 0;
-const GENRES_FOR_SONG = 1;  
+const GENRES_FOR_SONG = 1;
 
-class QueryFactory{
+class QueryFactory {
 
-    constructor(){
+    constructor() {
 
     }
 
     static get GENRE_INFO() {
         return GENRE_INFO;
-      }
-    
+    }
+
     static get GENRES_FOR_SONG() {
         return GENRES_FOR_SONG;
-      }
+    }
 
-    getQuery(queryType, entityValue){
-        switch(queryType){
+    getQuery(queryType, entityValue) {
+        switch (queryType) {
             case GENRE_INFO:
                 return this.buildQuery(genre_info_query, entityValue);
             case GENRES_FOR_SONG:
@@ -33,14 +33,10 @@ class QueryFactory{
         }
     }
 
-    buildQuery(query,entityValue){
-        return stringTemplate(query,"dbpInstance")(entityValue);
+    buildQuery(query, entityValue) {
+        return stringTemplate(query, "dbpInstance")(entityValue);
     }
 
 }
 
 module.exports = QueryFactory;
-
-
-const queryFactory = new QueryFactory();
-console.log(queryFactory.getQuery(QueryFactory.GENRE_INFO, "dbo:Starboy"));

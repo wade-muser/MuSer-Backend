409b4413a68b1729a30ebb35df16683c0ef319c1
xSkyripper <alexandru.cristea604@gmail.com>
2018-01-16 01:47:42 +0200

    Normalized content & escaped it

---
+++ services/commons/spotify_service.js
@@ -0,0 +1,106 @@
+require("dotenv").config({
+    path: "env/config.env"
+});
+const SpotifyWebAPI = require('spotify-web-api-node');
+
+class SpotifyService {
+
+    constructor(spotify_api) {
+        this.spotify_api = new SpotifyWebAPI({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        });
+
+    }
+
+    authorize() {
+        const promisified_function = (resolve, reject) => {
+            this.spotify_api.clientCredentialsGrant()
+                .then(data => {
+                    const access_token = data.body.access_token;
+                    this.spotify_api.setAccessToken(access_token);
+                    resolve();
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        };
+
+        return new Promise(promisified_function);
+    }
+
+    searchTrack(trackName, artistName) {
+        let promisified_function = (resolve, reject) => {
+            this.spotify_api.search(trackName + " " + artistName, ['track'])
+                .then(data => {
+                    let searchItem;
+                    // console.log(JSON.stringify(data.body.tracks));
+
+                    for (let item of data.body.tracks.items) {
+                        for (let artist of item.artists) {
+                            if (artist.name.toLowerCase() === artistName &&
+                                item.name.toLowerCase().includes(trackName)) {
+                                searchItem = item;
+                                resolve(searchItem);
+                                return;
+                            }
+                        }
+                    }
+                    resolve(searchItem);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        };
+        return new Promise(promisified_function);
+    }
+
+    searchArtist(artistName) {
+        let promisified_function = (resolve, reject) => {
+            this.spotify_api.search(artistName, ['artist'])
+                .then(data => {
+
+                    for (let item of data.body.artists.items) {
+                        if (item.name.toLowerCase() === artistName) {
+                            resolve(item);
+                            return;
+                        }
+                    }
+
+                    resolve(data);
+                })
+                .catch(err => {
+                    console.error(err);
+                    reject(err);
+                });
+        };
+
+        return new Promise(promisified_function);
+    }
+
+    searchAlbum(albumName, artistName) {
+        let promisified_function = (resolve, reject) => {
+            this.spotify_api.search(albumName, ['album'])
+                .then(data => {
+                    for (let item of data.body.albums.items) {
+                        for (let artist of item.artists) {
+                            if (artist.name.toLowerCase() === artistName &&
+                                item.name.toLowerCase().includes(albumName)) {
+                                resolve(item);
+                                return;
+                            }
+                        }
+                    }
+                    resolve({});
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        };
+
+        return new Promise(promisified_function);
+    }
+
+}
+
+module.exports = SpotifyService;

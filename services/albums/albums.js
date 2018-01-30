26eb7070ef3c7b528b19e1dc84c84c56b8b1df66
astonished12 <dan_cehan@yahoo.com>
2018-01-30 20:04:48 +0200

    album service recommendations

--- services/albums/albums.js
+++ services/albums/albums.js
@@ -86,13 +86,49 @@ module.exports.getAlbum = (event, context, callback) => {
 
 
 module.exports.getAlbumRecommendations = (event, context, callback) => {
+    console.log(event);
+    const queryStringIsInvalid = !event.queryStringParameters || !event.queryStringParameters.type;
+    if (queryStringIsInvalid) {
+        const AWSLambdaResponse = new HttpResponse.HttpResponseBuilder()
+            .statusCode(HTTP_STATUS_CODES.BAD_REQUEST)
+            .body({
+                message: "Parameters weren't provided"
+            })
+            .build()
+            .getLambdaResponse();
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: '/albums/{id}/recommended response',
-        }),
+        console.log("[ALBUMS] Response:", AWSLambdaResponse);
+        callback(null, AWSLambdaResponse);
+        return;
+    }
+
+    const id = event.pathParameters.id;
+    const type = event.queryStringParameters.type;
+    albumsService.getAlbumsRecommendation(id, type)
+        .then(results => {
+            console.log(results);
+            const AWSLambdaResponse = new HttpResponse.HttpResponseBuilder()
+                .statusCode(HTTP_STATUS_CODES.OK)
+                .body({
+                    results: results
+                })
+                .build()
+                .getLambdaResponse();
+
+            console.log("[ALBUMS] Response:", AWSLambdaResponse);
+            callback(null, AWSLambdaResponse);
+        })
+        .catch(err => {
+            console.log(err);
+            const AWSLambdaResponse = new HttpResponse.HttpResponseBuilder()
+                .statusCode(err.httpStatusCode || HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+                .body({
+                    message: "Some error occurred",
+                })
+                .build()
+                .getLambdaResponse();
+
+            console.log("[ALBUMS] Response:", AWSLambdaResponse);
+            callback(null, AWSLambdaResponse);
+        });
     };
-
-    callback(null, response);
-};
--- services/albums/albums_service.js
+++ services/albums/albums_service.js
@@ -5,6 +5,14 @@ const GraphDBMuserService = require("../commons/graphdb_muser_service");
 const CustomError = require("../../commons/utils/custom_error");
 const HTTP_STATUS_CODES = require("../../commons/utils/http_status_codes");
 
+const recommendationTypesToSparqlQuery = {
+    "sameartist": SparqlQueryFactory.FIND_ALBUM_RECOMMENDATION_TYPE_SAMEARTIST,
+    "sameyearandgenre": SparqlQueryFactory.FIND_ALUBM_RECOMMENATION_TYPE_SAMEYEARANDGENRE,
+    "samegenre": SparqlQueryFactory.FIND_ALBUM_RECOMMENDATION_TYPE_SAMEGENRE,
+    "relatedartist": SparqlQueryFactory.FIND_ALBUM_RECOMMENDATION_TYPE_RELATEDARTIST,
+};
+
+
 class AlbumsService {
 
     constructor() {
@@ -112,10 +120,64 @@ class AlbumsService {
         return new Promise(promisified_function);
     }
 
+
+    getAlbumsRecommendation(id, type) {
+        const promisified_function = (resolve, reject) => {
+            if (!recommendationTypesToSparqlQuery[type]) {
+                reject(new CustomError("Type isn't supported", HTTP_STATUS_CODES.BAD_REQUEST));
+                return;
+            }
+
+            async.waterfall([
+
+                /**
+                 * Function that retrieves the query that is need it
+                 */
+                (callback) => {
+                    const values = {
+                        id: this.getMuserEntity(id),
+                    };
+
+                    const query = this.queryFactory.getQuery(recommendationTypesToSparqlQuery[type], values);
+                    callback(null, query);
+                },
+
+                /**
+                 * Function that executes the query and returns the results
+                 */
+                (query, callback) => {
+                    console.log(query);
+                    this.graphdbMuserService.getQueryResults(query)
+                        .then(results => {
+                            console.log("Got results");
+                            callback(null, results);
+                        })
+                        .catch(err => {
+                            console.log("Error on get query results");
+                            console.log(err);
+                            callback(err);
+                        });
+                }
+
+
+            ], (err, results) => {
+                console.log(results);
+                if (err) {
+                    console.log(err);
+                    reject(err);
+                    return;
+                }
+                resolve(results);
+            });
+
+        };
+
+        return new Promise(promisified_function);
+    }
+
     getMuserEntity(id) {
         return `<http://example.com/muser#${id}>`;
     }
-
 }
 
 module.exports = AlbumsService;
